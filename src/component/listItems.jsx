9dba8bfe1c83eb72df7ec412d3b61135daeffd14
Gaurang Pant <70272876+GaurangHiHoon@users.noreply.github.com>
2022-02-19 21:08:54 +0530

    Update listItems.jsx

--- src/component/listItems.jsx
+++ src/component/listItems.jsx
@@ -2,7 +2,7 @@ import "./listitem.scss"
 
 const Listitem = () => {
     return ( <div className="listItem">
-     <video autoPlay="true" muted playsInline loop>
+     <video autoPlay muted playsInline loop>
          <source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-in-0819.m4v" type="video/mp4"/>
      </video>
     </div> );
